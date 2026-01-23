647e24e990897e7f6132a5582bed21a68cfe0c94
Cisco <cisco_vieira@hotmail.com>
2026-01-23 23:56:03 +0000

    Fix past sprints redirect locale

--- src/app/[locale]/sprints/past/page.tsx
+++ src/app/[locale]/sprints/past/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from '@/i18n/navigation';
 
-export default function PastSprintsRedirect() {
-  redirect('/sprints?view=list');
+export default function PastSprintsRedirect({ params }: { params: { locale: string } }) {
+  redirect({ href: '/sprints?view=list', locale: params.locale });
 }
